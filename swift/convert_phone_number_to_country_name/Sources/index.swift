c12f93acf5ecc8850638e80d57dc690d436a679d
Matej Bačo <matejbaco2000@gmail.com>
2022-03-04 14:15:03 +0000

    WIP: Swift convert phone number

---
+++ swift/convert_phone_number_to_country_name/Sources/index.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matej Bačo on 04/03/2022.
+//
+
+import Appwrite
+import AppwriteModels
+import Foundation
+
+func main(req: RequestValue, res: RequestResponse) async throws -> RequestResponse {
+    // TODO: Exact same logic as all others
+}
