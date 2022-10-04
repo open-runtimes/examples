5d30430f822392d9eab839bd19af6c6dc7c3e843
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-10-04 15:20:18 +0200

    feat: add url-shortener example

--- node/url-shortener/index.js
+++ node/url-shortener/index.js
@@ -8,15 +8,38 @@ module.exports = async function(req, res) {
 
    switch (provider) {
       case "bitly":
-         const bitlyClient = new bitly.BitlyClient(req.env.BITLY_API_KEY, {});
+         const bitlyClient = new bitly.BitlyClient(req.variables.BITLY_API_KEY, {});
          bitlyClient.shorten(url).then((url) => {
             res.json({
-               
+               success: true,
+               url: url.link
+            })
+         }).catch(() => {
+            res.json({
+               success: false,
+               message: "Entered URL is not a valid URL."
+            })
+         })
+
+         break;
+      case "tinyurl":
+         tinyurl.shorten(url).then((url) => {
+            res.json({
+               success: true,
+               url
+            })
+         }).catch(() => {
+            res.json({
+               success: false,
+               message: "Entered URL is not a valid URL."
             })
          })
 
          break;
       default:
-         break;
+         res.json({
+            success: false,
+            message: "The provider is invalid."
+         })
    }
 }
