0273e69f90594602d7efb62993d731ac54c17eb2
Radmacher <66096031+rdmchr@users.noreply.github.com>
2022-10-05 07:35:23 +0000

    Fixed Pagination & made deletion concurrent

--- node/wipe_appwrite_bucket/src/index.js
+++ node/wipe_appwrite_bucket/src/index.js
@@ -20,50 +20,73 @@ module.exports = async function (req, res) {
 
   // initialize appwrite client
   if (
-    !req.variables['APPWRITE_FUNCTION_ENDPOINT'] ||
-    !req.variables['APPWRITE_FUNCTION_API_KEY']
+    !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+    !req.variables["APPWRITE_FUNCTION_API_KEY"]
   ) {
-    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
+    console.warn(
+      "Environment variables are not set. Function cannot use Appwrite SDK."
+    );
     return;
   } else {
     client
-      .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
-      .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
-      .setKey(req.variables['APPWRITE_FUNCTION_API_KEY']);
+      .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+      .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+      .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"]);
   }
 
   // parse payload
   const payload = req.payload;
-  const {bucketId} = JSON.parse(payload);
+  const { bucketId } = JSON.parse(payload);
   if (!bucketId) {
     console.warn("Bucket ID is not set.");
     return;
   }
 
   // get files
-  let files;
-  try {
-    files = await storage.listFiles(bucketId);
-  } catch (err) {
-    console.warn(err);
-    res.json({success: false, message: "Bucket not found."});
-    return;
+  let filesFound = true;
+  let offset = 0;
+  let files = {
+    total: 0,
+    files: [],
+  };
+
+  while (filesFound) {
+    try {
+      // fetch files from appwrite
+      const sto = await storage.listFiles(bucketId, [
+        sdk.Query.limit(50),
+        sdk.Query.limit(0),
+      ]);
+      if (sto.total && sto.total > 0) {
+        // some files were found, save them for deletion
+        files.total += sto.total;
+        files.files += sto.files;
+
+        offset += files.total;
+      } else {
+        // no more files were found
+        filesFound = false;
+      }
+    } catch (err) {
+      console.warn(err);
+      res.json({ success: false, message: "Bucket not found." });
+      return;
+    }
   }
+
   if (!files.total || files.total === 0) {
     console.log("No files found.");
     return;
   }
 
-  // delete files
+  // add all deleteFile() requests to a list so we can run them all at once later
+  const deleteStack = [];
   for (const file of files.files) {
-    try {
-      await storage.deleteFile(bucketId, file.$id);
-    } catch (err) {
-      console.warn(err);
-      res.json({success: false, message: `Could not delete file ${file.$id}.`});
-      return;
-    }
+    deleteStack.push(await storage.deleteFile(bucketId, file.$id));
   }
+
+  const result = await Promise.allSettled(deleteStack);
+  //TODO: iterate over result to make sure every promise resolved correctly
 
   res.json({
     success: true,
