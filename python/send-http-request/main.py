379c83f45f45b743afe173f5bff65c12a676be4a
dejmedus <email96123@gmail.com>
2022-10-07 17:05:42 -0600

    sendHTTPRequest() Function using Python - add better error messages

--- python/send-http-request/main.py
+++ python/send-http-request/main.py
@@ -1,20 +1,43 @@
 import requests
 import json
 
+
+def errorMessage(res, message):
+    return res.json({
+        "success": False,
+        "message": message,
+    })
+    
+    
 def main(req,res):
+    
+    headers = None
+    body = None
     try:
         payload = json.loads(req.payload)
+        
+        #url and method are required
         url = payload['url']
         method = payload['method']
-        headers = payload['headers']
-        body = payload['body']
+        
+        # headers and body are optional
+        if 'headers' in payload.keys():
+            headers = payload['headers']
+        if 'body' in payload.keys():
+            body = payload['body']
 
-    except Exception as err:
-        print(err)
-        raise Exception('Please input a valid payload.')
+    except Exception:
+        return errorMessage(res, 'Payload must contain url and method.')
+    if url.strip() == "":
+        return errorMessage(res, 'Payload url cannot be empty.')
+    if method.strip() == "":
+        return errorMessage(res, 'Payload method cannot be empty.')
 
-    r = requests.request(method, url, headers=headers, json=body, timeout=30)
-    # r.raise_for_status()
+    r = None
+    try:
+        r = requests.request(method, url, headers=headers, json=body, timeout=30)
+    except Exception:
+        return errorMessage(res, 'URL could not be reached.')
     
     status = r.status_code
     if status == requests.codes.ok:
@@ -28,7 +51,4 @@ def main(req,res):
             }
         })
     else:
-        return res.json({
-            "success": False,
-            "message": "URL could not be reached.",
-        })
+        return errorMessage(res, f"{status} status error. Nothing to retrieve. Check that URL path is correct.")
