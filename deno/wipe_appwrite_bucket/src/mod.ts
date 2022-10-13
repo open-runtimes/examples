e02af97bfcb2aa455849c4bf13b31ebaafc9d05c
Simon <simongiesel@gmail.com>
2022-10-13 14:23:29 +0200

    loop over total files

--- deno/wipe_appwrite_bucket/src/mod.ts
+++ deno/wipe_appwrite_bucket/src/mod.ts
@@ -1,10 +1,10 @@
 import { sdk } from "./deps.ts";
 
-export default async function(req: any, res: any) {
-  if(
-    !req.variables['APPWRITE_FUNCTION_ENDPOINT'] || 
-    !req.variables['APPWRITE_FUNCTION_PROJECT_ID'] || 
-    !req.variables['APPWRITE_FUNCTION_API_KEY'] ||
+export default async function (req: any, res: any) {
+  if (
+    !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+    !req.variables["APPWRITE_FUNCTION_PROJECT_ID"] ||
+    !req.variables["APPWRITE_FUNCTION_API_KEY"] ||
     !req.payload
   ) {
     return res.json({
@@ -12,11 +12,11 @@ export default async function(req: any, res: any) {
       message: "Missing required environment variables or payload.",
     });
   }
-  let bucketId = '';
+  let bucketId = "";
   try {
     const payload = JSON.parse(req.payload);
     bucketId = payload.bucketId;
-  } catch(_err) {
+  } catch (_err) {
     return res.json({
       success: false,
       message: "Payload is invalid.",
@@ -24,28 +24,25 @@ export default async function(req: any, res: any) {
   }
   const client = new sdk.Client();
   const storage = new sdk.Storage(client);
-  
+
   client
-    .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
-    .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
-    .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
-  ;
+    .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+    .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+    .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"]);
 
   try {
-    const limit = 25;
-    let lastCursor = '';
-    let listFiles = await storage.listFiles(bucketId, [sdk.Query.limit(limit)]);
-    do {
-      lastCursor = listFiles.files[listFiles.files.length - 1].$id;
-      for(const file of listFiles.files) {
-        storage.deleteFile(bucketId, file.$id);
+    const limit = 1;
+    const totalFiles = (await storage.listFiles(bucketId, [sdk.Query.limit(limit)])).total;
+    for (let i = 0; i < totalFiles; i += limit) {
+      const files = (await storage.listFiles(bucketId, [sdk.Query.limit(limit)])).files;
+      for (const file of files) {
+        await storage.deleteFile(bucketId, file.$id);
       }
-      listFiles = await storage.listFiles(bucketId, [sdk.Query.limit(limit), sdk.Query.cursorAfter(lastCursor)]);
-    } while(listFiles.total > limit);
+    }
     return res.json({
       success: true,
     });
-  } catch(_err) {
+  } catch (_err) {
     return res.json({
       success: false,
       message: "Bucket not found.",
