06f5b775063838abb99defa57b9668aa2d219d54
Rinshin-Jalal <rinzhinjalal@gmail.com>
2022-10-15 23:50:00 +0530

    ⚡ Write a deepgramTranscribeVideo() Function using Python #4479

---
+++ python/deepgram-transcribe-video-python/main.py
@@ -0,0 +1,26 @@
+from deepgram import Deepgram
+import asyncio
+
+
+async def transribeVideo(api_key, payload):
+    dg_client = Deepgram(api_key)
+
+    source = {'url': payload['fileUrl']}
+
+    try:
+        response = await dg_client.transcription.prerecorded(source, {'punctuate': True, "model": 'video'})
+    except Exception as e:
+        return {'success': False, 'message': str(e)}
+
+    return {'success': True, 'deepgramData': response}
+
+
+def main(req, res):
+
+    payload = req.payload
+
+    api_key = req.variables['DEEPGRAM_API_KEY']
+
+    rs = asyncio.run(transribeVideo(api_key, payload))
+
+    return res.json(rs)
