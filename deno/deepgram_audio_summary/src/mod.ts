82aa48a2f1e6e36139a806b589f2e4cc4e75560c
vasucp1207 <vasucp1207@gmail.com>
2022-10-21 23:00:35 +0000

    Implement deepgramAudioSummary() function

---
+++ deno/deepgram_audio_summary/src/mod.ts
@@ -0,0 +1,35 @@
+export default async function (req: any, res: any) {
+    const APIkey = req.variables["DEEPGRAM_API_KEY"];
+    const fileUrl = req.payload["fileUrl"];
+
+    const response = await fetch(
+        "https://api.deepgram.com/v1/listen",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${APIkey}`,
+            },
+            body: JSON.stringify({ url: fileUrl }),
+        }
+    );
+
+    if (!APIkey || !fileUrl) {
+        return res.json('Please provide valid APIkey and fileUrl');
+    }
+
+    else {
+        const error = await response.json();
+        return res.json({
+            success: false,
+            message: error.reason,
+        });
+    }
+
+    const data = await response.json();
+
+    res.json({
+        success: true,
+        deepgramData: data,
+    });
+}
