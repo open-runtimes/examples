7335c2f19e340f04124c681c416f65d092af8b18
Farhan Ali Raza <farhanalirazaazeemi@gmail.com>
2022-10-25 22:32:48 +0500

    fixed issues in pr 52

--- deno/generate_website_screenshot/src/mod.ts
+++ deno/generate_website_screenshot/src/mod.ts
@@ -1,42 +1,29 @@
+import { base64Encode } from "./deps.ts";
 
-
-const getBase64FromUrl = async (url:any) => {
+const getBase64FromUrl = async (url: string) => {
   const data = await fetch(url);
+  if (data.ok) {
     const blob = await data.blob();
-    return new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(blob); 
-      reader.onloadend = () => {
-        const base64data = reader.result;   
-        resolve({status:data.status, base64String:base64data});
-      }
-    });
-    
+    const arr = await blob.arrayBuffer();
+    const base64String = base64Encode(arr);
+    return { success: true, screenshot: base64String };
+  } else {
+    return { success: false, screenshot: "" };
   }
+};
 
-
-export default async function(req: any, res: any) {
-  const APIkey = req.variables["SCREEN_SHOT_API_KEY"]
-
-    const { url } = JSON.parse(req.payload);
-
-    if (!url) {
-        throw new Error("url is required");
-    }
-
-    if (!APIkey) {
-        throw new Error("API key is required");
-    }
-
-
-  const fetchURL = `https://shot.screenshotapi.net/screenshot?token=${APIkey}&url=${url}&output=image&file_type=png&wait_for_event=load`
-  const response:any = await getBase64FromUrl(fetchURL)
-  // Check status code
-  if (response.status !== 200) {
-      res.json({"success":false,"message":"Website could not be reached."})
-      return;
+export default async function (req: any, res: any) {
+  const APIkey = req.variables["SCREEN_SHOT_API_KEY"];
+  const { url } = JSON.parse(req.payload);
+  if (!url || !APIkey) {
+    res.json({ success: false, message: "Website could not be reached." });
+    return;
   }
-
-  // Parse the response  
-  res.json({"success":true, "screenshot":response.base64String});
+  const fetchURL = `https://shot.screenshotapi.net/screenshot?token=${APIkey}&url=${url}&output=image&file_type=png&wait_for_event=load`;
+  const response:{success:boolean, screenshot:string} = await getBase64FromUrl(fetchURL);
+  if (response.success) {
+    res.json(response);
+    return;
+  }
+  res.json({ success: false, message: "Website could not be reached." });
 }
