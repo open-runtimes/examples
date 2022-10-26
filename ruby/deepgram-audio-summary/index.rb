18810a63ef12f6e2482450390669824baf701628
Azanul <azanulhaque@gmail.com>
2022-10-26 07:23:28 +0000

    review changes

--- ruby/deepgram-audio-summary/index.rb
+++ ruby/deepgram-audio-summary/index.rb
@@ -17,26 +17,32 @@ def main(req, res)
   url = URI("https://api.deepgram.com/v1/listen?summarize=true&punctuate=true")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
   request = Net::HTTP::Post.new(url)
   request["content-type"] = "application/json"
   request["Authorization"] = "Token #{req.env["DEEPGRAM_API_KEY"]}"
-  request.body = "{\"url\":\"#{fileUrl}\"}"
+  request.body = JSON.generate({url: fileUrl})
 
   begin
     response = http.request(request)
   rescue Exception => err
     res.json({
       success: false,
-      deepgramData: "Please provide a valid file URL."
+      message: err
     })
   end
 
+ if response.code=="200"
   parsed_response = JSON.parse(response.read_body)
-  summaries = parsed_response["results"]["channels"][0]["alternatives"][0]["summaries"]
   res.json({
     success: true,
-    deepgramData: summaries
+    deepgramData: parsed_response
   })
+ else
+  res.json({
+    success: false,
+    message: response.body
+  })
+ end
 end
