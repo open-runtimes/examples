4606c163b65eb21e7f742fc106ba400786dcff55
Ambareen09 <ambubushra@gmail.com>
2022-10-26 22:55:17 +0530

    `index.rb` UPDATE

--- ruby/deepgram-transcribe-video/index.rb
+++ ruby/deepgram-transcribe-video/index.rb
@@ -10,31 +10,41 @@ def main(req, res)
     payload = JSON.parse(req.payload)
     fileUrl = payload["fileUrl"]
   rescue Exception => err
-      puts err
-      raise "Payload is invalid."
+    res.json({
+      success: false,
+      message: "Payload is invalid."
+    })
   end
+  
   url = URI("https://api.deepgram.com/v1/listen?model=video")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
   request = Net::HTTP::Post.new(url)
   request["content-type"] = "application/json"
   request["Authorization"] = "Token #{req.env["DEEPGRAM_API_KEY"]}"
-  request.body = "{\"url\":\"#{fileUrl}\"}"
+  request.body = JSON.generate({url: fileUrl})
 
   begin
     response = http.request(request)
   rescue Exception => err
     res.json({
       success: false,
-      deepgramData: "Please provide a valid file URL."
+      message: err
     })
   end
+
+ if response.code=="200"
   parsed_response = JSON.parse(response.read_body)
-  transcript = parsed_response["results"]["channels"][0]["alternatives"][0]["transcript"]
   res.json({
     success: true,
-    deepgramData: transcript
+    deepgramData: parsed_response["results"]
   })
+ else
+  res.json({
+    success: false,
+    message: response.body
+  })
+ end
 end
