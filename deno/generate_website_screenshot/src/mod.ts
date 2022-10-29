3b9862d79d4a69ce9c94549223de5158eea7fe1c
Farhan Ali Raza <farhanalirazaazeemi@gmail.com>
2022-10-30 00:35:13 +0500

    fixed pr problems

--- deno/generate_website_screenshot/src/mod.ts
+++ deno/generate_website_screenshot/src/mod.ts
@@ -6,24 +6,29 @@ const getBase64FromUrl = async (url: string) => {
     const blob = await data.blob();
     const arr = await blob.arrayBuffer();
     const base64String = base64Encode(arr);
-    return { success: true, screenshot: base64String };
+    return { success: true, screenshot: base64String, error: "" };
   } else {
-    return { success: false, screenshot: "" };
+    return { success: false, screenshot: "", error: data.statusText };
   }
 };
 
 export default async function (req: any, res: any) {
   const APIkey = req.variables["SCREEN_SHOT_API_KEY"];
   const { url } = JSON.parse(req.payload);
-  if (!url || !APIkey) {
-    res.json({ success: false, message: "Website could not be reached." });
+  if (!url) {
+    res.json({ success: false, message: "URL is required" });
+    return;
+  }
+  if (!APIkey) {
+    res.json({ success: false, message: "API key is required" });
     return;
   }
   const fetchURL = `https://shot.screenshotapi.net/screenshot?token=${APIkey}&url=${url}&output=image&file_type=png&wait_for_event=load`;
-  const response:{success:boolean, screenshot:string} = await getBase64FromUrl(fetchURL);
+  const response: { success: boolean; screenshot: string; error: string } =
+    await getBase64FromUrl(fetchURL);
   if (response.success) {
-    res.json(response);
+    res.json({ success: true, screenshot: response.screenshot });
     return;
   }
-  res.json({ success: false, message: "Website could not be reached." });
+  res.json({ success: false, message: response.error });
 }
