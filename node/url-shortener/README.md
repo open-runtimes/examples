5b8f9aa166b7d1b3f8a84f2a7c81d97358f33300
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-12-03 17:43:43 +0100

    fix: multiple corrections

--- node/url-shortener/README.md
+++ node/url-shortener/README.md
@@ -22,9 +22,9 @@ _Example output:_
 }
 ```
 
-## 📝 Environment Variables
+## 📝 Variables
 
-With the Bitly provider, you need an API key with the name: BITLY_API_KEY.
+A single variable: `BITLY_API_KEY`
 
 ## 🚀 Deployment
 
@@ -37,17 +37,17 @@ $ cd node/url-shortener
 
 2. Enter this function folder and build the code:
 ```
-docker run --rm --interactive --tty --volume $PWD:/usr/code openruntimes/node:17.0 sh /usr/local/src/build.sh
+docker run --rm --interactive --tty --volume $PWD:/usr/code openruntimes/node:v2-18.0 sh /usr/local/src/build.sh
 ```
 As a result, a `code.tar.gz` file will be generated.
 
 3. Start the Open Runtime:
 ```
-docker run -p 3000:3000 -e INTERNAL_RUNTIME_ENTRYPOINT=src/index.js -e INTERNAL_RUNTIME_KEY=secret-key --rm --interactive --tty --volume $PWD/code.tar.gz:/tmp/code.tar.gz:ro openruntimes/node:17.0 sh /usr/local/src/start.sh
+docker run -p 3000:3000 -e INTERNAL_RUNTIME_KEY=secret-key -e INTERNAL_RUNTIME_ENTRYPOINT=index.js --rm --interactive --tty --volume $PWD/code.tar.gz:/tmp/code.tar.gz:ro openruntimes/node:v2-18.0 sh /usr/local/src/start.sh
 ```
 
 Your function is now listening on port `3000`, and you can execute it by sending `POST` request with appropriate authorization headers. To learn more about runtime, you can visit NodeJS runtime [README](https://github.com/open-runtimes/open-runtimes/tree/main/runtimes/node-17.0).
 
 ## 📝 Notes
  - This function is designed for use with Appwrite Cloud Functions. You can learn more about it in [Appwrite docs](https://appwrite.io/docs/functions).
- - This example is compatible with NodeJS 17.0. Other versions may work but are not guarenteed to work as they haven't been tested.
+ - This example is compatible with NodeJS 18.0. Other versions may work but are not guarenteed to work as they haven't been tested.
--- node/url-shortener/index.js
+++ node/url-shortener/index.js
@@ -1,45 +1,65 @@
 const bitly = require("bitly")
 const tinyurl = require("turl")
+
 module.exports = async function(req, res) {
-   const {
-      provider,
-      url
-   } = JSON.parse(req.payload)
+   const { provider, url } = req.payload
+   if (!provider) {
+      return res.json({
+        success: false,
+        message: "The provider is missing."
+      })
+   }
+
+   if (!url) {
+      return res.json({
+         success: false,
+         message: "The link is missing."
+      })
+   }
 
    switch (provider) {
-      case "bitly":
-         const bitlyClient = new bitly.BitlyClient(req.variables.BITLY_API_KEY, {});
-         bitlyClient.shorten(url).then((url) => {
-            res.json({
-               success: true,
-               url: url.link
+      case "bitly": {
+         const bitlyClient = new bitly.BitlyClient(req.variables.BITLY_API_KEY, {})
+         bitlyClient.shorten(url)
+            .then((url) => {
+               return res.json({
+                  success: true,
+                  url: url.link
+               })
             })
-         }).catch(() => {
-            res.json({
-               success: false,
-               message: "Entered URL is not a valid URL."
+            .catch((e) => {
+               return res.json({
+                  success: false,
+                  message: e.toString()
+               })
             })
-         })
 
-         break;
-      case "tinyurl":
-         tinyurl.shorten(url).then((url) => {
-            res.json({
-               success: true,
-               url
-            })
-         }).catch(() => {
-            res.json({
-               success: false,
-               message: "Entered URL is not a valid URL."
-            })
-         })
+            break
+      }
 
-         break;
-      default:
+      case "tinyurl": {
+         tinyurl.shorten(url)
+            .then((url) => {
+               return res.json({
+                  success: true,
+                  url
+               })
+            })
+            .catch((e) => {
+               return res.json({
+                  success: false,
+                  message: e.toString()
+               })
+            })
+
+            break
+      }
+
+      default: {
          res.json({
             success: false,
             message: "The provider is invalid."
          })
+      }
    }
 }
