4ea3b7c9035cd88a85b2823d9e1fd24083bbf75b
vasucp1207 <vasucp1207@gmail.com>
2022-12-12 11:48:49 +0000

    refactoring

--- deno/deepgram_audio_summary/src/mod.ts
+++ deno/deepgram_audio_summary/src/mod.ts
@@ -1,40 +1,59 @@
 export default async function (req: any, res: any) {
-    const APIkey = req.variables["DEEPGRAM_API_KEY"];
-    let fileUrl = "";
-
     try {
-        fileUrl = JSON.parse(req.payload)["fileUrl"];
-    } catch (error) {
-        return res.json("Invalid JSON string");
-    }
-
-    const response = await fetch(
-        "https://api.deepgram.com/v1/listen?summarize=true&punctuate=true",
+      const apikey = req.variables["DEEPGRAM_API_KEY"];
+      const fileUrl = JSON.parse(req.payload)["fileUrl"];
+  
+      if (!apikey) {
+        return res.json({
+          success: false,
+          message: "Missing Deepgram API key",
+        });
+      }
+  
+      if (!fileUrl) {
+        return res.json({
+          success: false,
+          message: "Missing fileUrl",
+        });
+      }
+  
+      const response = await fetch(
+        "https://api.deepgram.com/v1/listen?",
         {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Token ${APIkey}`,
-            },
-            body: JSON.stringify({ url: fileUrl }),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token ${apikey}`,
+          },
+          body: JSON.stringify({ url: fileUrl }),
         }
-    );
-
-    if (!APIkey || !fileUrl) {
-        return res.json("Please provide valid APIkey and fileUrl");
-    }
-
-    else if(response.status !== 201) {
+      );
+  
+      if (response.status !== 200) {
+        if (response.status === 401) {
+          return res.json({
+            success: false,
+            message: `Encountered an error trying to make a request: ${response.statusText}`,
+          });
+        }
+  
         const error = await response.json();
         return res.json({
-            success: false,
-            message: `Encountered an error trying to make a request: ${error.reason}`,
+          success: false,
+          message: error.reason,
         });
-    }
-
-    const data = await response.json();
-    res.json({
+      }
+  
+      const data = await response.json();
+  
+      return res.json({
         success: true,
         deepgramData: data,
-    });
-}
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
