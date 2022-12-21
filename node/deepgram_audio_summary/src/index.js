e258f931cccfec2e08ecd3ef986bd863f4897ba5
Apoorv Dwivedi <apoorv@Apoorvs-MacBook-Pro.local>
2022-12-21 22:54:08 +0530

    added deepgram_audio_summary function

---
+++ node/deepgram_audio_summary/src/index.js
@@ -0,0 +1,36 @@
+const axios = require('axios').default;
+
+module.exports = async (req, res) => {
+  const APIkey = req.env['DEEPGRAM_API_KEY'];
+  const { fileUrl } = req.payload;
+
+  if (!APIkey) {
+    throw new Error('Deepgram API Key is required');
+  }
+
+  if (!fileUrl) {
+    throw new Error('fileUrl is required');
+  }
+
+  const resp = await axios.post(
+    'https://api.deepgram.com/v1/listen?summarize=true&punctuate=true',
+    {
+      url: fileUrl,
+    },
+    {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Token ${APIkey}`,
+      },
+    },
+  );
+
+  if (resp.status === 200) {
+    res.json({
+      success: true,
+      deepgramData: resp.data.results,
+    });
+  } else {
+    res.json({ success: false, message: 'Please provide a valid file URL.' });
+  }
+};
