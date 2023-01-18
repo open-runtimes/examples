e8c1a4ea757b8aa2258ba2e5b95442e2bedcb33e
Matej Bačo <matejbaco2000@gmail.com>
2023-01-18 13:03:40 +0000

    Finish readme generation script

--- .github/workflows/examples-list-workflow/index.js
+++ .github/workflows/examples-list-workflow/index.js
@@ -1,5 +1,44 @@
-const fs = require("fs");
-const path = require("path");
+import * as fs from "fs";
+import * as path from "path";
+import {markdownTable} from 'markdown-table'
+
+
+const folderDenylist = [ '.github', '.git' ];
+
+const runtimes = fs.readdirSync(path.join('.', '../../../'), { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+    .filter((folder) => !folderDenylist.includes(folder));
+
+const examples = [];
+
+for(const runtime of runtimes) {
+    const folders = fs.readdirSync(path.join('.', `../../../${runtime}`), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+    examples.push(...folders);
+}
+
+const uniqueExamples = [...new Set(examples)];
+
+const rows = uniqueExamples.map((example) => {
+    const languagesSupport = runtimes.map((runtime) => {
+        return fs.existsSync(path.join('.', `../../../${runtime}/${example}`)) ? '✅' : '';
+    })
+
+    return [example, ...languagesSupport];
+});
+
+const table = markdownTable([
+    ['Example', ...runtimes],
+    ...rows.sort((a, b) => {
+        const aCount = a.filter((column) => column === '✅');
+        const bCount = b.filter((column) => column === '✅');
+
+        return aCount > bCount ? -1 : 1;
+    })
+  ]);
+
 
 const readmePath = path.join(__dirname, "../../../README.md");
 const readme = fs.readFileSync(readmePath).toString();
@@ -7,11 +46,11 @@ let newReadme = '';
 
 if(readme.includes('<!-- EXAMPLES-LIST:START -->') && readme.includes('<!-- EXAMPLES-LIST:END -->')) {
     newReadme += readme.split('<!-- EXAMPLES-LIST:START -->')[0];
-    newReadme += '\n';
+    newReadme += '<!-- EXAMPLES-LIST:START -->\n';
 
-    newReadme += 'THIS WILL BE GENERATED 🥳';
+    newReadme += table;
 
-    newReadme += '\n';
+    newReadme += '\n<!-- EXAMPLES-LIST:END -->';
     newReadme += readme.split('<!-- EXAMPLES-LIST:END -->')[1];
 }
 
