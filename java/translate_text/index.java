483439cf3a7b9074dd930565d901b063a6191f12
Ayush Goyal <ayush_goyal@intuit.com>
2023-04-02 01:52:23 +0530

    4147 - Write a translateText() Function using Java

--- java/translate_text/index.java
+++ java/translate_text/index.java
@@ -12,6 +12,9 @@ import software.amazon.awssdk.services.translate.TranslateClient;
 import software.amazon.awssdk.services.translate.model.TranslateException;
 import software.amazon.awssdk.services.translate.model.TranslateTextRequest;
 import software.amazon.awssdk.services.translate.model.TranslateTextResponse;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import okhttp3.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,7 +27,7 @@ import java.util.*;
 
 final Gson gson = new Gson();
 
-public RuntimeResponse main(RuntimeRequest req, RuntimeResponse res) {
+public RuntimeResponse main(RuntimeRequest req, RuntimeResponse res) throws Exception {
     // Validate that values present in the request are not empty (payload, variables)
     RuntimeResponse errorResponse = checkEmptyPayloadAndVariables(req, res);
     if (errorResponse != null) {
@@ -45,21 +48,23 @@ public RuntimeResponse main(RuntimeRequest req, RuntimeResponse res) {
     String provider = payload.get("provider").toString();
     String sourceLanguageCode = payload.get("from").toString();
 
-    // Name API key is not empty
     String API_KEY = "API_KEY";
+    String REGION = "REGION";
+    String SECRET_ACCESS_KEY = "SECRET_ACCESS_KEY";
 
-    errorResponse = validateVariables(req, res, API_KEY, provider);
+    errorResponse = validateVariables(req, res, API_KEY, SECRET_ACCESS_KEY, REGION, provider);
     if (errorResponse != null) {
         return errorResponse;
     }
-    Map<String, Object> variables = gson.fromJson(payloadString, Map.class);
-    String apiKey = variables.get(API_KEY).toString();
+
+    Map<String, String> variables = req.getVariables();
+    String apiKey = variables.get(API_KEY);
 
     String translatedText = "";
     Map<String, Object> responseData = new HashMap<>();
 
     try {
-        translatedText = translateText(payload, apiKey);
+        translatedText = translateText(payload, variables, apiKey);
     } catch (Exception e) {
         responseData.put("success", false);
         responseData.put("message", "Something went wrong while translating the text, please check with the developers. Error: " + e.getMessage());
@@ -67,23 +72,25 @@ public RuntimeResponse main(RuntimeRequest req, RuntimeResponse res) {
     }
     responseData.put("success", true);
     responseData.put("message", translatedText);
-    responseData.put("from",sourceLanguageCode);
+    responseData.put("from", sourceLanguageCode);
 
     return res.json(responseData);
 }
 
-public String translateText(Map<String, Object> payload, String apiKey) throws GeneralSecurityException, IOException {
+public String translateText(Map<String, Object> payload, Map<String, String> variables, String apiKey) throws GeneralSecurityException, IOException {
     String provider = payload.get("provider").toString();
     String sourceLanguageCode = payload.get("from").toString();
     String targetLanguageCode = payload.get("to").toString();
     String text = payload.get("text").toString();
 
-    if (Objects.equals(Provider.AWS.toString(), provider)) {
-        return translateTexteUsingAWS(text, sourceLanguageCode, targetLanguageCode, apiKey);
-    } else if (Objects.equals(Provider.GOOGLE.toString(), provider)) {
+    if (Objects.equals("aws", provider)) {
+        String secretAccessKey = variables.get("SECRET_ACCESS_KEY");
+        return translateTexteUsingAWS(text, sourceLanguageCode, targetLanguageCode, apiKey, secretAccessKey);
+    } else if (Objects.equals("google", provider)) {
         return translateTextUsingGCP(text, targetLanguageCode, apiKey);
-    } else if (Objects.equals(Provider.AZURE.toString(), provider)) {
-        return translateTexteUsingAzure(text, targetLanguageCode, apiKey);
+    } else if (Objects.equals("azure", provider)) {
+        String region = variables.get("REGION");
+        return translateTexteUsingAzure(text, sourceLanguageCode, targetLanguageCode, apiKey, region);
     }
 
     return null;
@@ -102,13 +109,13 @@ public String translateTextUsingGCP(String text, String targetLanguage, String a
     return translationsResourceList.get(0).getTranslatedText();
 }
 
-public String translateTexteUsingAWS(String text, String sourceLanguageCode, String targetLanguageCode, String apiKey) {
+public String translateTexteUsingAWS(String text, String sourceLanguageCode, String targetLanguageCode, String accessKeyId, String secretAccessKey) {
 
     try {
         Region region = Region.US_WEST_2;
         TranslateClient translateClient = TranslateClient.builder()
                 .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
                 .build();
 
         TranslateTextRequest textRequest = TranslateTextRequest.builder()
@@ -128,38 +135,51 @@ public String translateTexteUsingAWS(String text, String sourceLanguageCode, Str
     return text;
 }
 
-public String translateTexteUsingAzure(String text, String targetLanguageCode, String apiKey) throws IOException {
-    String endpointUrl = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=" + targetLanguageCode;
-    String requestBody = "[{\n\t\"text\": " + text + "\n}]";
-    URL url = new URL(endpointUrl);
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("POST");
-    con.setRequestProperty("Content-Type", "application/json");
-    con.setRequestProperty("Ocp-Apim-Subscription-Key", apiKey);
-    con.setRequestProperty("Accept", "application/json");
-    con.setDoOutput(true);
-    OutputStream os = con.getOutputStream();
-    byte[] input = requestBody.getBytes("utf-8");
-    os.write(input, 0, input.length);
-    StringBuilder response = new StringBuilder();
-    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
-    String responseLine = null;
-    while ((responseLine = br.readLine()) != null) {
-        response.append(responseLine.trim());
-    }
-    br.close();
-    con.disconnect();
-    return response.toString();
+public String translateTexteUsingAzure(String text, String sourceLanguageCode, String targetLanguageCode, String apiKey, String region) throws IOException {
+    String endpointUrl = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=" + sourceLanguageCode + "&to=" + targetLanguageCode;
+    String requestBody = "[{\"text\":\"" + text + "\"}]";
+
+    MediaType mediaType = MediaType.parse("application/json");
+    RequestBody body = RequestBody.create(mediaType,
+            requestBody);
+    Request request = new Request.Builder()
+            .url(endpointUrl)
+            .post(body)
+            .addHeader("Ocp-Apim-Subscription-Key", apiKey)
+            // location required if you're using a multi-service or regional (not global) resource.
+            .addHeader("Ocp-Apim-Subscription-Region", region)
+            .addHeader("Content-type", "application/json")
+            .build();
+    Response response = client.newCall(request).execute();
+    String res = response.body().string();
+    JsonParser parser = new JsonParser();
+    JsonElement json = parser.parse(res);
+    String answer = json.getAsJsonArray().getAsJsonArray().get(0).getAsJsonObject().get("translations").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString();
+
+    return answer;
 }
 
-private RuntimeResponse validateVariables(RuntimeRequest req, RuntimeResponse res, String apiKey, String provider) {
+private RuntimeResponse validateVariables(RuntimeRequest req, RuntimeResponse res, String apiKey, String secretAccessKey, String region, String provider) {
     Map<String, String> variables = req.getVariables();
 
-    if (!Objects.equals(Provider.AWS.toString(), provider) && !variables.containsKey(apiKey) || variables.get(apiKey) == null || variables.get(apiKey).trim().isEmpty()) {
+    if (!variables.containsKey(apiKey) || variables.get(apiKey) == null || variables.get(apiKey).trim().isEmpty()) {
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("success", false);
-        responseData.put("message", "Please pass a non-empty API Key(s) {}" + apiKey + " for translateText");
+        responseData.put("message", "Please pass a non-empty API Key(s) " + apiKey + " for translateText");
+        return res.json(responseData);
+    }
 
+    if (Objects.equals("aws", provider) && (!variables.containsKey(secretAccessKey) || variables.get(secretAccessKey) == null || variables.get(secretAccessKey).trim().isEmpty())) {
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("success", false);
+        responseData.put("message", "Please pass a non-empty " + secretAccessKey + " for translateText");
+        return res.json(responseData);
+    }
+
+    if (Objects.equals("azure", provider) && (!variables.containsKey(region) || variables.get(region) == null || variables.get(region).trim().isEmpty())) {
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("success", false);
+        responseData.put("message", "Please pass a non-empty " + region + " for translateText");
         return res.json(responseData);
     }
 
@@ -241,13 +261,13 @@ private RuntimeResponse checkEmptyPayloadAndVariables(RuntimeRequest req, Runtim
 
     if (req.getPayload() == null || req.getPayload().trim().isEmpty() || req.getPayload().trim().equals("{}")) {
         responseData.put("success", false);
-        responseData.put("message", "Payload is empty, expected a payload with bucketId");
+        responseData.put("message", "Payload is empty, expected a payload with {\"provider\":\"aws\",\"from\":\"cs\",\"to\":\"en\",\"text\":\"Ahoj svet.\"}");
         return res.json(responseData);
 
     }
     if (req.getVariables() == null) {
         responseData.put("success", false);
-        responseData.put("message", "Empty function variables found. You need to pass an API key.");
+        responseData.put("message", "Empty function variables found. You need to pass an API_KEY");
         return res.json(responseData);
     }
 
