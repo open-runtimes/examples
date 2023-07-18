92f2aebe785efe299566f21cbfcfdbdf85a9e384
gyamposudodzi <gyamposu@gmail.com>
2023-07-18 03:50:35 +0000

    changed name from Twiliosms to send_sms_twilio

--- dotnet/sendMessage/functions/send_sms_twilio.cs
+++ dotnet/sendMessage/functions/send_sms_twilio.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-public class TwilioSMS
+public class Send_sms_twilio
 {
     public static async Task<object> SendSMS(Dictionary<string, string> variables, string phoneNumber, string message)
     {
