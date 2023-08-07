ce468a1c756c4adeb63d2df1f7772439234d8c91
Elizabeth Barco <elizabethbarco1@gmail.com>
2023-08-07 19:44:58 +0000

    collection_wiper_elizabeth

--- java/AppwriteCollectionWiper/Index.java
+++ java/AppwriteCollectionWiper/Index.java
@@ -1,96 +1,92 @@
 import java.util.Map;
-import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.*;
+import io.openruntimes.java.*;
 import com.google.gson.Gson;
 import io.appwrite.Client;
-import io.appwrite.services.Databases;
 import io.appwrite.exceptions.AppwriteException;
-import io.appwrite.models.DocumentList;
+import io.appwrite.services.Databases;
+import kotlin.Result;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.EmptyCoroutineContext;
+import org.jetbrains.annotations.NotNull;
+import okhttp3.Response;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
 
 final Gson gson = new Gson();
 
+
 public RuntimeResponse main(RuntimeRequest req, RuntimeResponse res) throws Exception {
-        var client = new Client();
-        var database = new Databases(client);
+  
+    
+    
+    // Initialize the Appwrite client and databases service
+    var client = new Client();
+    var databases = new Databases(client);
+    var variables = req.getVariables();
+    var payloadString = req.getPayload();
 
-        var variables = req.getVariables();
-
-        System.out.println("In the jungle ******");
-
-        if (variables == null
+    // Check if the required environment variables are set
+    if (variables == null
             || !variables.containsKey("APPWRITE_FUNCTION_ENDPOINT")
             || !variables.containsKey("APPWRITE_FUNCTION_API_KEY")
             || !variables.containsKey("APPWRITE_FUNCTION_PROJECT_ID")
             || variables.get("APPWRITE_FUNCTION_ENDPOINT") == null
             || variables.get("APPWRITE_FUNCTION_API_KEY") == null
             || variables.get("APPWRITE_FUNCTION_PROJECT_ID") == null) {
-            return res.json(Map.of(
-                    "success", false,
-                    "message", "Variables missing."
-            ));
-        } else {
-            client
+        return res.json(Map.of("Environment variables are not set. Function cannot use Appwrite SDK.", false));
+    } else {
+        // Set the Appwrite client properties
+        client
                 .setEndpoint(variables.get("APPWRITE_FUNCTION_ENDPOINT"))
                 .setProject(variables.get("APPWRITE_FUNCTION_PROJECT_ID"))
                 .setKey(variables.get("APPWRITE_FUNCTION_API_KEY"));
+    }
+
+    try {
+        // Parse the payload data into a Map
+        
+        Map<String, Object> payload = gson.fromJson(payloadString, Map.class);
+        String databaseId = (String) payload.get("databaseId");
+        String collectionId = (String) payload.get("collectionId");
+         if (payload == null || databaseId == null || collectionId == null) {
+            return res.json(Map.of("Invalid payload.", false));
         }
+            
+       
+       
+       databases.deleteCollection(
+            databaseId,
+           collectionId,
+            new Continuation<Object>() {
+                @NotNull
+                @Override
+                public CoroutineContext getContext() {
+                    return EmptyCoroutineContext.INSTANCE;
+                }
 
-        try {
-            JsonObject payload = gson.fromJson(req.getPayload(), JsonObject.class);
-            if (!payload.has("databaseId") || !payload.has("collectionId")) {
-            return res.json(Map.of(
-                "success", false,
-                "message", "Invalid payload."
-            ));
-        }
-
-        int sum = 0;
-        boolean done = false;
-
-        while (!done) {
-            Response<DocumentList> response = database.listDocuments(
-                payload.get("databaseId").getAsString(),
-                payload.get("collectionId").getAsString()
-            ).execute();
-
-            if (response.getStatusCode() != 200) {
-                throw new AppwriteException("Failed to list documents");
-            }
-
-            DocumentList result = response.getBody();
-
-            if (result == null || result.getDocuments() == null) {
-                done = true;
-                break;
-            }
-
-            for (var document : result.getDocuments()) {
-                Response<Void> deleteResponse = database.deleteDocument(
-                    payload.get("databaseId").getAsString(),
-                    payload.get("collectionId").getAsString(),
-                    document.get("$id").getAsString()
-                ).execute();
-
-                if (deleteResponse.getStatusCode() == 200) {
-                    sum++;
-                } else {
-                    throw new AppwriteException("Failed to delete document");
+                @Override
+                public void resumeWith(@NotNull Object o) {
+                    String json = "";
+                    try {
+                        if (o instanceof Result.Failure) {
+                            Result.Failure failure = (Result.Failure) o;
+                            throw failure.exception;
+                        } else {
+                            Response response = (Response) o;
+                        }
+                    } catch (Throwable th) {
+                        System.out.println("ERROR: " +  th.toString());
+                    }
                 }
             }
+        );
+        return  res.json(Map.of("Success", true));
 
-            if (result.getDocuments().size() == 0) {
-                done = true;
-            }
-        }
-
-        return res.json(Map.of(
-            "success", true,
-            "sum", sum
-        ));
-        } catch (AppwriteException e) {
-            return res.json(Map.of(
-                "success", false,
-                "message", "Unexpected error: " + e.getMessage()
-            ));
-        }
+    }catch (AppwriteException e) {
+        
+        return res.json(Map.of("Collection not found", false));   
+         }
 }
-
